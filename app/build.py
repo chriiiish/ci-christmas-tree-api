419dd80063eca94b7769d0e011cdb7822379ae1a
Chris Lloyd <chris@Chriss-iMac.local>
2018-11-30 14:09:38 +1300

    Added Create Method

--- app/build.py
+++ app/build.py
@@ -1,2 +1,26 @@
-def create(buildid):
-    return buildid
+import boto3
+import json
+
+#/createbuild/{buildId}
+def create(event, context):
+    send(event["pathParameters"]["buildId"], 1)
+    return{ "statusCode": 200 }
+
+"""
+Sends a message to a topic
+build_id = The ID of the build
+status = 1 - Create, 2 - Success, 3 - Failure
+topic = name of topic to send to
+"""
+def send(build_id, status, topic="buildmasterchristmastree"):
+    client = boto3.client("iot-data")
+    payload = json.dumps({
+        "buildId": build_id,
+        "status": status
+    })
+    client.publish(
+        topic=topic,
+        qos=1,
+        payload=payload
+    )
+    return 1
